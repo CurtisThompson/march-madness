b83b526ba42080b04987335c19096ca9ec9479f8
Curtis Thompson <curtiswaynethompson@gmail.com>
2023-03-11 20:24:16 +0000

    Choose pipeline components from config

--- run_pipeline.py
+++ run_pipeline.py
@@ -20,10 +20,9 @@ from src.model.predict import run as predict_current_year
 from src.submission.kaggle_submission import submit as competition_submit
 
 
-CONFIG = get_config()
+def run(CONFIG):
+    """Run complete ML pipeline."""
 
-
-if __name__ == "__main__":
     # Build data directory structure
     folders = [
         './data/etl/',
@@ -39,33 +38,45 @@ if __name__ == "__main__":
             os.makedirs(folder)
 
     # Ingestion
-    #download_kaggle(comp=CONFIG['kaggle']['competition'])
-    #download_538(start_year=CONFIG['etl']['538_start_year'], end_year=CONFIG['etl']['538_end_year'])
+    if CONFIG['run_component']['ingestion']:
+        download_kaggle(comp=CONFIG['kaggle']['competition'])
+        download_538(start_year=CONFIG['etl']['538_start_year'], end_year=CONFIG['etl']['538_end_year'])
 
     # ETL
-    find_gender()
-    save_win_ratios()
-    calculate_clutch_win_ratio(max_score_gap=CONFIG['etl']['clutch_score_gap_max'])
-    find_538_ratings(start_year=CONFIG['etl']['538_start_year'], end_year=CONFIG['etl']['538_end_year'])
-    reformat_seeds()
-    get_teams_form()
-    #calculate_elo(K=CONFIG['etl']['elo_k_factor'])
+    if CONFIG['run_component']['etl']:
+        # Build features
+        find_gender()
+        save_win_ratios()
+        calculate_clutch_win_ratio(max_score_gap=CONFIG['etl']['clutch_score_gap_max'])
+        find_538_ratings(start_year=CONFIG['etl']['538_start_year'], end_year=CONFIG['etl']['538_end_year'])
+        reformat_seeds()
+        get_teams_form()
+        if CONFIG['run_component']['etl_elo']:
+            calculate_elo(K=CONFIG['etl']['elo_k_factor'])
 
-    # Build Datasets
-    build_training_set(elo_K=CONFIG['etl']['elo_k_factor'], start_year=CONFIG['etl']['training_start_year'])
-    build_test_set(elo_K=CONFIG['etl']['elo_k_factor'])
+        # Build Datasets
+        build_training_set(elo_K=CONFIG['etl']['elo_k_factor'], start_year=CONFIG['etl']['training_start_year'])
+        build_test_set(elo_K=CONFIG['etl']['elo_k_factor'])
 
     # Build Models
-    validate_and_build_model(training_columns_men=CONFIG['model']['mens_columns'],
-                             training_columns_women=CONFIG['model']['womens_columns'],
-                             tune=CONFIG['model']['is_tune'],
-                             random_state=CONFIG['random_state'],
-                             optimisation_iterations=CONFIG['model']['optimisation_iterations'],
-                             optimisation_initial_points=CONFIG['model']['optimisation_initial'])
+    if CONFIG['run_component']['training']:
+        validate_and_build_model(training_columns_men=CONFIG['model']['mens_columns'],
+                                 training_columns_women=CONFIG['model']['womens_columns'],
+                                 tune=CONFIG['run_component']['tuning'],
+                                 random_state=CONFIG['random_state'],
+                                 optimisation_iterations=CONFIG['model']['optimisation_iterations'],
+                                 optimisation_initial_points=CONFIG['model']['optimisation_initial'])
 
     # Predictions
-    predict_current_year(model_columns_men=CONFIG['model']['mens_columns'],
-                         model_columns_women=CONFIG['model']['womens_columns'])
+    if CONFIG['run_component']['prediction']:
+        predict_current_year(model_columns_men=CONFIG['model']['mens_columns'],
+                             model_columns_women=CONFIG['model']['womens_columns'])
 
     # Submit Predictions To Competition
-    #competition_submit(comp=CONFIG['kaggle']['competition'], message=CONFIG['kaggle']['submit_message'])
+    if CONFIG['run_component']['submit_prediction']:
+        competition_submit(comp=CONFIG['kaggle']['competition'], message=CONFIG['kaggle']['submit_message'])
+
+
+if __name__ == "__main__":
+    CONFIG = get_config()
+    run(CONFIG)
