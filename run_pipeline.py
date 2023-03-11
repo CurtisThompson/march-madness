3ab0f2bf85514281c02ba2ec46e30947b802062b
Curtis Thompson <curtiswaynethompson@gmail.com>
2023-03-11 19:39:15 +0000

    All pipeline params from config

--- run_pipeline.py
+++ run_pipeline.py
@@ -23,7 +23,6 @@ from src.submission.kaggle_submission import submit as competition_submit
 CONFIG = get_config()
 
 
-
 if __name__ == "__main__":
     # Build data directory structure
     folders = [
@@ -40,27 +39,33 @@ if __name__ == "__main__":
             os.makedirs(folder)
 
     # Ingestion
-    #download_kaggle()
+    #download_kaggle(comp=CONFIG['kaggle']['competition'])
     #download_538(start_year=CONFIG['etl']['538_start_year'], end_year=CONFIG['etl']['538_end_year'])
 
     # ETL
     find_gender()
     save_win_ratios()
-    calculate_clutch_win_ratio()
+    calculate_clutch_win_ratio(max_score_gap=CONFIG['etl']['clutch_score_gap_max'])
     find_538_ratings(start_year=CONFIG['etl']['538_start_year'], end_year=CONFIG['etl']['538_end_year'])
     reformat_seeds()
     get_teams_form()
     #calculate_elo(K=CONFIG['etl']['elo_k_factor'])
 
     # Build Datasets
-    build_training_set(elo_K=CONFIG['etl']['elo_k_factor'])
+    build_training_set(elo_K=CONFIG['etl']['elo_k_factor'], start_year=CONFIG['etl']['training_start_year'])
     build_test_set(elo_K=CONFIG['etl']['elo_k_factor'])
 
     # Build Models
-    validate_and_build_model()
+    validate_and_build_model(training_columns_men=CONFIG['model']['mens_columns'],
+                             training_columns_women=CONFIG['model']['womens_columns'],
+                             tune=CONFIG['model']['is_tune'],
+                             random_state=CONFIG['random_state'],
+                             optimisation_iterations=CONFIG['model']['optimisation_iterations'],
+                             optimisation_initial_points=CONFIG['model']['optimisation_initial'])
 
     # Predictions
-    predict_current_year()
+    predict_current_year(model_columns_men=CONFIG['model']['mens_columns'],
+                         model_columns_women=CONFIG['model']['womens_columns'])
 
     # Submit Predictions To Competition
-    #competition_submit(message=CONFIG['kaggle']['submit_message'])
+    #competition_submit(comp=CONFIG['kaggle']['competition'], message=CONFIG['kaggle']['submit_message'])
