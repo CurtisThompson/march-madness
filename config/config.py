36f05470895e62c143a4e1e6ea30626e3a3f60b5
Curtis Thompson <curtiswaynethompson@gmail.com>
2023-03-22 19:11:55 +0000

    Docstring for config

--- config/config.py
+++ config/config.py
@@ -1,7 +1,15 @@
 import yaml
 
 def get_config(config_path='./config/config.yaml'):
-    """Imports a config file."""
+    """
+    Import a config file.
+
+    Args:
+        config_path: Path to yaml file that contains configuration.
+    
+    Returns:
+        Dictionary of configuration key-value pairs.
+    """
     with open(config_path, 'r') as f:
         config = yaml.safe_load(f)
     return config
